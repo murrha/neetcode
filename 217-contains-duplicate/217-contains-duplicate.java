ff2848359d3a4a497d43091a1a89cddc712a0de6
marja katrina <38115150+murrha@users.noreply.github.com>
2022-10-01 14:39:55 -0700

    Time: 15 ms (62.33%), Space: 70.9 MB (31.01%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(int n: nums){
+            if(!set.add(n))
+                return true;
+        }
+        
+        return false; 
+    }
+}
